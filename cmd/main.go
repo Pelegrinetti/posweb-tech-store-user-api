296c9da856fa5a6ef13066205444807120ac94c4
Pelegrinetti <pelegrinetti.dev@gmail.com>
2022-04-10 15:34:47 -0300

    Apply container for database

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"github.com/Pelegrinetti/posweb-user-api/internal/config"
+	"github.com/Pelegrinetti/posweb-user-api/pkg/container"
+	"github.com/Pelegrinetti/posweb-user-api/pkg/database"
 	"github.com/Pelegrinetti/posweb-user-api/pkg/server"
+	"github.com/sirupsen/logrus"
 )
+
+func checkError(err error) {
+	if err != nil {
+		logrus.Fatal(err)
+	}
+}
 
 func main() {
 	cfg := config.New()
-	srv := server.New(cfg.ServerConfig)
+
+	db, dbError := database.New(cfg.DatabaseConfig)
+
+	ctn := container.New()
+
+	ctn.AddDatabase(db)
+
+	srv := server.New(ctn, cfg.ServerConfig)
+
+	checkError(dbError)
 
 	srv.Run()
 }
