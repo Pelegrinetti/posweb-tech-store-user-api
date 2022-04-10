5c4ab4d6bae8b6d28bef6448d91b51f77204cde5
Pelegrinetti <pelegrinetti.dev@gmail.com>
2022-04-10 15:33:04 -0300

    Add database config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,16 +1,19 @@
 package config
 
 import (
+	"github.com/Pelegrinetti/posweb-user-api/pkg/database"
 	"github.com/Pelegrinetti/posweb-user-api/pkg/server"
 	"github.com/spf13/viper"
 )
 
 type Config struct {
-	ServerConfig server.ServerConfig `mapstructure:",squash"`
+	ServerConfig   server.ServerConfig     `mapstructure:",squash"`
+	DatabaseConfig database.DatabaseConfig `mapstructure:",squash"`
 }
 
 func New() Config {
 	viper.SetDefault("PORT", 3001)
+	viper.SetDefault("MONGODB_URI", "mongodb://localhost:27017/user-api")
 	viper.AutomaticEnv()
 
 	config := Config{}
