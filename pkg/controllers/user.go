4708c19aae4a6ae30c1264806ee08236124ab67a
Pelegrinetti <pelegrinetti.dev@gmail.com>
2022-04-24 12:20:30 -0300

    Create update user handler

--- pkg/controllers/user.go
+++ pkg/controllers/user.go
@@ -45,3 +45,27 @@ func CreateUser(db *database.Database) fiber.Handler {
 		return c.Status(201).Send(parsedUser)
 	}
 }
+
+func UpdateUser(db *database.Database) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		usersCollection := db.GetCollection("users")
+
+		user := models.NewUser(usersCollection)
+
+		_, findError := user.FindOne(c.Params("id"))
+
+		if findError != nil {
+			logrus.Error("Can't find user: ", findError)
+		}
+
+		parsedUser, parsingError := json.Marshal(user)
+
+		if parsingError != nil {
+			logrus.Error("Parsing user to JSON: ", parsingError)
+
+			return c.SendStatus(500)
+		}
+
+		return c.Status(201).Send(parsedUser)
+	}
+}
